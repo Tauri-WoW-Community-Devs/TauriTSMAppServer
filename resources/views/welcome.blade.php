bade7f3420bdbe018ae0f72c3758b850ec040153
DimaSheiko <dimasheiko11@gmail.com>
2020-07-01 08:42:19 -0400

    Changed the location of AppData.lua for heroku purposes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="/storage/AppData.lua" download>Download File</a>
+                    <a href="AppData.lua" download>Download File</a>
                 </div>
             </div>
         </div>
