2ff1d86214e14c2d8a33bbb4e6c6f2ffdab9f3af
DimaSheiko <dimasheiko11@gmail.com>
2020-07-09 01:43:10 -0400

    Deleted facade from route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
